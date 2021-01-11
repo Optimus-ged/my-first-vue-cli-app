92e21bc299a97467e13feb4b0e317aec93fcca34
Optimus-ged <gedeonyalakuhanda@gmail.com>
2021-01-11 17:51:10 +0200

    modify

--- my-first-vue-cli-app/src/App.vue
+++ my-first-vue-cli-app/src/App.vue
@@ -2,7 +2,6 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia necessitatibus ad totam iste voluptates asperiores? Ea iusto totam quo, necessitatibus praesentium commodi doloribus sunt eos odit quidem? Animi, aspernatur dolorum.</p>
   </div>
 </template>
 
